459e2ca667bf91581032966d8bfcf90b9242618e
Claire Carlotti <Claire.Calotti@softwire.com>
2022-07-05 13:31:22 +0100

    Add Fizz

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -6,7 +6,14 @@
         {
             for (int i = 1; i <= 100; i++)
             {
-                Console.WriteLine(i);
+                if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
             }
         }
     }
