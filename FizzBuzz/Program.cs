41ddf0f47259e14cfb492bad25e266328d55f1ac
Claire Carlotti <Claire.Calotti@softwire.com>
2022-07-05 15:49:44 +0100

    Rules chosen by user from list

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -16,30 +16,39 @@
         
         static void CountFizzBuzz()
         {
-            int min = RequestInteger("Choose a minimum integer: ", 1);
-            int max = RequestInteger("Choose a maximum integer: ", min);
+            int min = RequestInteger("Choose a starting integer: ", 1);
+            int max = RequestInteger("Choose a finishing integer: ", min);
+            int ruleProduct = 1;
+            int rule = 1;
+            Console.WriteLine("(3)Fizz  (5)Buzz  (7)Bang  (11)Bong  (13)Fezz  (17)Reverse");
+            do
+            {
+                ruleProduct *= rule;
+                rule = RequestInteger("Please enter the integer corresponding to a rule you would like to implement," +
+                                      " or enter 0 if there are no further rules you would like to add: ", -1);
+            } while (rule != 0);
             
             for (int i = min; i <= max; i++)
             {
                 string display = "";
-                if (i % 3 == 0)
+                if (i % 3 == 0 & ruleProduct % 3 == 0)
                 {
                     display += "Fizz";
                 }
-                if (i % 5 == 0)
+                if (i % 5 == 0 & ruleProduct % 5 == 0)
                 {
                     display += "Buzz";
                 }
-                if (i % 7 == 0)
+                if (i % 7 == 0 & ruleProduct % 7 == 0)
                 {
                     display += "Bang";
                 }
-                if (i % 11 == 0)
+                if (i % 11 == 0 & ruleProduct % 11 == 0)
                 {
                     display = "Bong";
                 }
 
-                if (i % 13 == 0)
+                if (i % 13 == 0 & ruleProduct % 13 == 0)
                 {
                     string tempFezzDisplay = display;
                     for (int j = 0; j < tempFezzDisplay.Length; j++)
@@ -55,7 +64,7 @@
                     }
                 }
 
-                if (i % 17 == 0)
+                if (i % 17 == 0 & ruleProduct % 17 == 0)
                 {
                     string tempRevDisplay = display;
                     display = "";
